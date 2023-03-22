1b0c973845053b6c3948fa3f62f21745ec0cf102
Ben Donnelly <ben_donnelly@intergral.com>
2023-03-22 21:07:51 +0000

    update build to use otel protoc image

--- scripts/python_build.sh
+++ scripts/python_build.sh
@@ -2,18 +2,20 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-PROJECT_ROOT="$(dirname "SCRIPT_DIR")"
+PROJECT_ROOT="$(realpath $(dirname "SCRIPT_DIR"))"
 
 OUT_DIR="${PROJECT_ROOT}/build"
 
 PYTHON_OUT="${OUT_DIR}/python"
 
+PROTOC=${PROTOC:="docker run --rm -u 1000 -v${PROJECT_ROOT}:${PROJECT_ROOT} -w${PROJECT_ROOT} otel/build-protobuf:latest"}
+
 rm -Rf ${PYTHON_OUT}
 
 mkdir -p ${PYTHON_OUT}/src
 
-for f in $(find ${PROJECT_ROOT}/deep -name '*.proto'); do python -m grpc_tools.protoc  -I ${PROJECT_ROOT} --python_out=${PYTHON_OUT}/src $f; done
+for f in $(find ${PROJECT_ROOT}/deepproto -name '*.proto'); do ${PROTOC} --proto_path ${PROJECT_ROOT} --python_out=${PYTHON_OUT}/src $f; done
 
-python -m grpc_tools.protoc -I ${PROJECT_ROOT} --python_out=${PYTHON_OUT}/src --grpc_python_out=${PYTHON_OUT}/src ${PROJECT_ROOT}/deep/proto/poll/v1/poll.proto
+eval "${PROTOC} --proto_path ${PROJECT_ROOT} --python_out=${PYTHON_OUT}/src --grpc-python_out=${PYTHON_OUT}/src ${PROJECT_ROOT}/deepproto/proto/poll/v1/poll.proto"
 
 cp pyproject.toml ${PYTHON_OUT}

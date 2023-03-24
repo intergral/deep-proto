9eaa713ad8de2466fd6c69e09ab1b1e5cadb2b1a
Ben Donnelly <ben_donnelly@intergral.com>
2023-03-24 00:24:21 +0000

    add tracepoint and snapshot definitions

--- scripts/python_build.sh
+++ scripts/python_build.sh
@@ -14,8 +14,11 @@ rm -Rf ${PYTHON_OUT}
 
 mkdir -p ${PYTHON_OUT}/src
 
+# All proto files need to be processed as proto
 for f in $(find ${PROJECT_ROOT}/deepproto -name '*.proto'); do ${PROTOC} --proto_path ${PROJECT_ROOT} --python_out=${PYTHON_OUT}/src $f; done
 
+# only proto files with services need to be generated with grpc
 eval "${PROTOC} --proto_path ${PROJECT_ROOT} --python_out=${PYTHON_OUT}/src --grpc-python_out=${PYTHON_OUT}/src ${PROJECT_ROOT}/deepproto/proto/poll/v1/poll.proto"
+eval "${PROTOC} --proto_path ${PROJECT_ROOT} --python_out=${PYTHON_OUT}/src --grpc-python_out=${PYTHON_OUT}/src ${PROJECT_ROOT}/deepproto/proto/tracepoint/v1/tracepoint.proto"
 
 cp pyproject.toml ${PYTHON_OUT}
